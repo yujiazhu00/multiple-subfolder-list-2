9f68ebf6b84dc56a5f5b34b08f819cfac649ce7b
yujiazhu00 <83460885+yujiazhu00@users.noreply.github.com>
2022-03-21 00:59:24 +0800

    Update code.py

--- code.py
+++ code.py
@@ -52,6 +52,8 @@ def multiple_folders(input_dir, output_name):
     sheet.cell(row=1, column=3).value = 'Description of Document'
     for z in range(1,4):
         sheet.cell(row=1, column=z).font = Font(bold=True)
+    sheet.cell(row=1, column=4).value = 'Old File name'
+    sheet.cell(row=1, column=5).value = 'New File name'
     for folderName, subfolders, filenames in os.walk(input_dir):
         y = y + [folderName]
     length_y = len(y)
@@ -60,8 +62,8 @@ def multiple_folders(input_dir, output_name):
     length_y_new = len(y)
     for i in range(0, length_y_new):
         p11 = Path(y[i])
-        dir_path = os.listdir(p11)
-        dir_path = [os.path.splitext(x)[0] for x in dir_path]
+        dir_path_extension = os.listdir(p11)
+        dir_path = [os.path.splitext(x)[0] for x in dir_path_extension]
         my_date_list = extract_date(dir_path)
         my_name_list = extract_name(dir_path)
         length_list = len(my_date_list)
@@ -75,6 +77,8 @@ def multiple_folders(input_dir, output_name):
                 sheet.cell(row=k + initial_value, column=1).value = str(serial_number) + '.'
                 sheet.cell(row=k + initial_value, column=2).value = my_date_list[k - 1]
                 sheet.cell(row=k + initial_value, column=3).value = my_name_list[k - 1]
+                sheet.cell(row=k + initial_value, column=4).value = dir_path_extension[k - 1]
+                sheet.cell(row=k + initial_value, column=5).value = str(serial_number) + ". " + dir_path_extension[k - 1]
                 serial_number = serial_number + 1
             else:
                 initial_value = initial_value + k
